9d8e3cde356f5ae5fadc46081c3a058c86ceab4e
chuckis <chuckis94@gmail.com>
2021-03-31 19:33:50 +0300

    login

--- bookservice/bookservice/urls.py
+++ bookservice/bookservice/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('shelf/', include('shelf.urls')),
     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
