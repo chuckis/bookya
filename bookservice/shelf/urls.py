f0c962e6cd17bb2e27529ab0b0d503c523265f78
chuckis <chuckis94@gmail.com>
2021-04-01 19:08:59 +0300

    base, login templates

--- bookservice/shelf/urls.py
+++ bookservice/shelf/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from shelf.views import BookList, BookCreate, BookUpdate, BookDelete
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', BookList.as_view(), name='book-list'),
-    path('add/', BookCreate.as_view(), name='book-add'),
-    path('<int:pk>/', BookUpdate.as_view(), name='book-update'),
-    path('<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
+    path('', login_required(BookList.as_view()), name='book-list'),
+    path('add/', login_required(BookCreate.as_view()), name='book-add'),
+    path('<int:pk>/', login_required(BookUpdate.as_view()), name='book-update'),
+    path('<int:pk>/delete/', login_required(BookDelete.as_view()), name='book-delete'),
 ]
