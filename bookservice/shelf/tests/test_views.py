49176336111350eb7303cfcc8b06c33d95ceabaa
chuckis <chuckis94@gmail.com>
2021-04-20 21:03:16 +0300

    red test

--- bookservice/shelf/tests/test_views.py
+++ bookservice/shelf/tests/test_views.py
@@ -9,9 +9,21 @@ pytestmark = pytest.mark.django_db
 import shelf.views as views
 
 
-class TestBookList:
+class TestBookListView:
     def test_anonymous(self):
         req = RequestFactory().get('/shelf')
         resp = views.BookList.as_view()(req)
         assert resp.status_code == 200, 'Should be callable by anyone'
 
+class TestIndexView:
+    """
+    - книга,
+    - когда отдали на чтение
+    - кому
+    - когда вернут
+    """
+    def test_anonymous(self):
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = views.IndexView.as_view()(req)
+        assert 'login' in resp.url, 'Should redirect to login'
